5bd456819bb127ec3d3d14974453a0f4664a38df
angelatan222 <ayt2110@barnard.edu>
2022-06-21 15:45:50 -0400

    app with pages

--- app.R
+++ app.R
@@ -1,39 +1,105 @@
-
+#install.packages("shiny")
 library(shiny)
+#install.packages("leaflet")
+#install.packages("sf")
 library(ggplot2)
+library(sf)
+library(leaflet)
+#install.packages("tigris")
+library(tigris)
+
+#modzcta shapefiles from NYC open data
+unzip("Modified Zip Code Tabulation Areas (MODZCTA).zip")
+modzcta <- st_read("geo_export_9d592ba8-7629-4558-9e78-e1e536b453d9.shp")
+head(modzcta)
+
+df1 <- read.csv("rats_cleaned.csv")
+df <- df1 %>%
+  filter(Year > 2014)
+#percent of calls in each zip code
+total <- sum(df$count) #total 311 calls
+df <- df %>%
+  group_by(zip) %>%
+  summarize(percent = (sum(count)/total)*100) 
+df$zip<-as.character(df$zip)
+
+all_modzcta <- geo_join(modzcta, df, "modzcta", "zip", how = "inner")
+saveRDS(all_modzcta, "all_modzcta.RDS")
+
+all_modzcta <-all_modzcta %>%
+  st_transform(crs = "+init=epsg:4326")
+
+#Palettes and Labels for percent map
+labels_percent <-sprintf(
+  "<strong>%s</strong><br/>%g%% ",
+  all_modzcta$modzcta,all_modzcta$percent) %>%
+  lapply(htmltools::HTML)
+
+pal <-colorBin(palette="Reds", 9, domain = all_modzcta$percent)
 
 
-df <- read.csv("Data/rats_cleaned.csv")
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# 3 tabs
+ui <- navbarPage("NYC Rat Population Estimate",
   
-  titlePanel("Rat Estimate"),
-  
-  numericInput("zip", label = "Which ZIP code are you interested in?", value = 00000),
-  
-  plotOutput("rats")
-  
+    tabPanel("Rat Sighting Reports by ZIP",
+             leafletOutput("percent_map")),
+    
+    tabPanel("Estimated Rat Population by ZIP",
+              sidebarLayout(
+                sidebarPanel(
+                  numericInput("zip", 
+                  label = "Which ZIP code are you interested in?", 
+                  value = 00000)
+               ),
+               mainPanel(
+                 plotOutput("rats")),
+               )
+             ),
+    tabPanel("Other Data Visualizations")
 )
 
-server <- function(input, output) {
-  
-  output$rats <- renderPlot({
-    df1 <- df %>% 
-      filter(zip == input$zip) %>%
-      group_by(Year) %>%
-      summarize(Count = sum(count))
+
+server <- function(input, output, session) {
+
+    output$percent_map <- renderLeaflet({  
+      leaflet(all_modzcta) %>%
+        addProviderTiles(provider = "CartoDB.Positron") %>%
+        addPolygons(label = labels_percent,
+                    stroke = FALSE,
+                    smoothFactor = 0.5,
+                    opacity = 1,
+                    fillOpacity = 0.7,
+                    fillColor = ~pal(percent),
+                    highlightOptions = highlightOptions(
+                      weight = 5,
+                      color = "black",
+                      fillOpacity = 1,
+                      opacity = 1,
+                      bringToFront = TRUE)
+        )%>%
+        addLegend("topleft",
+                  pal = pal,
+                  values = ~percent,
+                  title = "% of Total 311 Rat Sightings Reported",
+                  opacity = 0.7)
+ 
+    })
     
-    ggplot(df1, aes(x=Year, y=Count)) +
-      geom_bar(stat="identity") +
-      labs(title = paste("Estimated Rat Population in the zip code: ", input$zip)) +
-      theme(panel.grid = element_blank(), 
-            axis.line.x = element_line(), 
-            axis.line.y = element_line(), 
-            panel.background = element_rect(fill = "white"))
-  })
+    output$rats <- renderPlot({
+      plotRats <- df1 %>% 
+        filter(zip == input$zip) %>%
+        group_by(Year) %>%
+        summarize(Count = sum(count))
+      
+      ggplot(plotRats, aes(x=Year, y=Count)) +
+        geom_bar(stat="identity") +
+        labs(title = paste("Estimated Rat Population in the zip code: ", input$zip)) +
+        theme(panel.grid = element_blank(), 
+              axis.line.x = element_line(), 
+              axis.line.y = element_line(), 
+              panel.background = element_rect(fill = "white"))
+    })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
