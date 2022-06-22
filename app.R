45b496e36d63089b7bbe52251c84bb736f186627
angelatan222 <ayt2110@barnard.edu>
2022-06-22 14:12:00 -0400

    update with mari's work

--- app.R
+++ app.R
@@ -12,18 +12,21 @@ library(tigris)
 unzip("Modified Zip Code Tabulation Areas (MODZCTA).zip")
 modzcta <- st_read("geo_export_9d592ba8-7629-4558-9e78-e1e536b453d9.shp")
 
-df1 <- read.csv("rats_cleaned.csv")
-df <- df1 %>%
+df <- read.csv("rats_cleaned.csv")
+
+#ORGANIZING DATA FOR PERCENT MAP (angela)
+
+df <- df %>%
   filter(Year > 2014)
 
 #percent of calls in each zip code
 total <- sum(df$count) #total 311 calls
-df <- df %>%
+rats_percent <- df %>%
   group_by(zip) %>%
   summarize(percent = (sum(count)/total)*100) 
-df$zip<-as.character(df$zip)
+rats_percent$zip<-as.character(rats_percent$zip)
 
-all_modzcta <- geo_join(modzcta, df, "modzcta", "zip", how = "inner")
+all_modzcta <- geo_join(modzcta, rats_percent, "modzcta", "zip", how = "inner")
 all_modzcta <-all_modzcta %>%
   st_transform(crs = "+init=epsg:4326")
 
@@ -35,6 +38,31 @@ labels_percent <-sprintf(
 
 pal <-colorBin(palette="Reds", 9, domain = all_modzcta$percent)
 
+
+#ORGANIZING DATA FOR POPULATION MAP (mari)
+# creating 2019 and 2020 dataframes
+zips_year_1 <- df %>% filter(Year == 2019) 
+zips_year_2 <- df %>% filter(Year == 2020)
+
+# estimate number of rats in each zipcode
+count_per_zip <- function(zipcode){
+  num_y1 <- zips_year_1 %>% filter(zip == zipcode)
+  num_y2 <- zips_year_2 %>% filter (zip == zipcode)
+  number_in_year_1 <- sum(num_y1$count)
+  #print(number_in_year_1)
+  number_in_year_2 <- sum(num_y2$count)
+  #print(number_in_year_2)
+  number_in_year_1_and_2 <- sum(num_y1$count %in% num_y2$count)
+  #print(number_in_year_2)
+  50 * number_in_year_1 * number_in_year_2 / number_in_year_1_and_2
+}
+
+rats_pop <- df %>%
+  group_by(zip) %>%
+  summarize(total = count_per_zip(zip))  
+rats_pop$zip<-as.character(rats_pop$zip)
+
+all_modzcta_pop <- geo_join(modzcta, rats_pop, "modzcta", "zip", how = "inner")
 
 # 3 tabs
 ui <- navbarPage("NYC Rat Population Estimate",
@@ -84,7 +112,7 @@ server <- function(input, output, session) {
     })
     
     output$rats <- renderPlot({
-      plotRats <- df1 %>% 
+      plotRats <- df %>% 
         filter(zip == input$zip) %>%
         group_by(Year) %>%
         summarize(Count = sum(count))
